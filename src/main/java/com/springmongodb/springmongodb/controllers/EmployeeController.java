717040a0feb940850cc0f6c08908a9f861a3bcdb
Phillips III, Ernest Alvin CTR USA <ernest.a.phillips4.ctr@mail.mil>
2021-08-31 08:09:53 -0400

    added employee add page and methods in controller

--- src/main/java/com/springmongodb/springmongodb/controllers/EmployeeController.java
+++ src/main/java/com/springmongodb/springmongodb/controllers/EmployeeController.java
@@ -8,6 +8,8 @@ import com.springmongodb.springmongodb.models.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -15,7 +17,7 @@ public class EmployeeController {
   //map of employee records
   public Map<String, Employee> employees = new HashMap<>();
 
-  //create some employee objects
+  //create employee objects
   public EmployeeController() {
     employees.put("E101", new Employee("E101", "Ernie", "Phillips", 35));
     employees.put("E102", new Employee("E102", "Katie", "Phillips", 36));
@@ -23,11 +25,28 @@ public class EmployeeController {
     employees.put("E104", new Employee("E104", "Kelsey", "Phillips", 2));
   }
 
-
+  //get all employee records
   @GetMapping("/employee-list")
   public String getAllEmployees(Model model) {
     //put employees in the model object
     model.addAttribute("employees", employees.values());
+    return "employee-list";//string must match html file naem
+  }
+
+  
+
+  //return form for adding employee
+  @GetMapping("/employee/add")
+  public String addEmployee(){
+    return "add-employee";
+  }
+
+  //post employee record
+  @PostMapping("/employee/add")
+  public String addEmployee(@ModelAttribute("addEmployee") Employee employee, Model model){
+    employees.put(employee.getId(), employee);
+    model.addAttribute("employees", employees.values());
     return "employee-list";
   }
+  
 }
