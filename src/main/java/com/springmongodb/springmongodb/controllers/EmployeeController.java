faeb19e1a2888dfb962fd659fd3926ffffdff749
Phillips III, Ernest Alvin CTR USA <ernest.a.phillips4.ctr@mail.mil>
2021-08-31 09:35:21 -0400

    added the delete employee feature

--- src/main/java/com/springmongodb/springmongodb/controllers/EmployeeController.java
+++ src/main/java/com/springmongodb/springmongodb/controllers/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -30,7 +31,7 @@ public class EmployeeController {
   public String getAllEmployees(Model model) {
     //put employees in the model object
     model.addAttribute("employees", employees.values());
-    return "employee-list";//string must match html file naem
+    return "list-employee";//string must match html file naem
   }
 
   
@@ -46,7 +47,32 @@ public class EmployeeController {
   public String addEmployee(@ModelAttribute("addEmployee") Employee employee, Model model){
     employees.put(employee.getId(), employee);
     model.addAttribute("employees", employees.values());
-    return "employee-list";
+    return "list-employee";
   }
-  
+
+  //get page for edit employee
+  @GetMapping("/employee/edit/{id}")
+  public String editEmployee(@PathVariable("id") String empId, Model model){
+    model.addAttribute("editEmployee", employees.get(empId));
+    return "edit-employee";
+  }
+
+  //post - update the employee record
+  @PostMapping("/employee/edit/{id}")
+  public String updateEmployee(@PathVariable("id") String empId, @ModelAttribute("editEmployee") Employee employee, Model model){
+    employees.put(empId, employee);
+    model.addAttribute("employees", employees.values());
+    return "list-employee";
+  }
+
+  //delete method
+  @GetMapping("/employee/delete/{id}")
+  public String deleteEmployee(@PathVariable("id") String empId, Model model){
+    //remove employee
+    employees.remove(empId);
+
+    //get list of employees and return to list page
+    model.addAttribute("employees", employees.values());
+    return "list-employee";
+  }
 }
