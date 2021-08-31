ef3a1867771864d3688a8c65c8dad2cec60c02da
Phillips III, Ernest Alvin CTR USA <ernest.a.phillips4.ctr@mail.mil>
2021-08-31 19:06:53 -0400

    adding the encrypted db values, need the decrypt as a param in vmargs in launch.json

--- src/main/java/com/springmongodb/springmongodb/SpringmongodbApplication.java
+++ src/main/java/com/springmongodb/springmongodb/SpringmongodbApplication.java
@@ -1,9 +1,12 @@
 package com.springmongodb.springmongodb;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //https://www.geeksforgeeks.org/how-to-encrypt-passwords-in-a-spring-boot-project-using-jasypt/
+@EnableEncryptableProperties
 public class SpringmongodbApplication {
 
 	public static void main(String[] args) {
